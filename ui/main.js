caf9dea80032307134d4681afa20559182896282
HimmShekhar <30702151+HimmShekhar@users.noreply.github.com>
2017-08-17 21:01:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ var element = document.getElementById("main text");
 element.innerHTML="Real Madrid halamadrid";
 
 //Move the image
-var img=document.getElementById("img");
-img.onclick= function(){
+var img=document.getElementById("madi");
+madi.onclick= function(){
     
     img.style.marginleft="125px";
 };
