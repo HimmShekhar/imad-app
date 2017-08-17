a26b01cd3a0230e553f2caa45371022e579cd552
HimmShekhar <30702151+HimmShekhar@users.noreply.github.com>
2017-08-17 21:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
 var element = document.getElementById("main text");
 element.innerHTML="Real Madrid halamadrid";
+
+//Move the image
+var img=document.getElementById("img");
+img.onclick= function(){
+    
+    img.style.marginleft="125px";
+};
+
+
