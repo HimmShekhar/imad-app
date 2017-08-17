dfe900dbceb1e4e9d40f80500b9cc38bba785921
HimmShekhar <30702151+HimmShekhar@users.noreply.github.com>
2017-08-17 21:15:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,12 @@ element.innerHTML="Real Madrid halamadrid";
 var img=document.getElementById("madi");
 marginLeft=0;
 function moveRight () {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 2;
     img.style.marginLeft=marginLeft+'px';
     
 }
 img.onclick= function(){
-    var interval= setInterval(moveRight,125);
+    var interval= setInterval(moveRight,50);
     img.style.marginLeft="125px";
 }; 
 
