4291039950f6ef00833d3e5d9786a87ba70f20b3
HimmShekhar <30702151+HimmShekhar@users.noreply.github.com>
2017-08-17 12:18:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,71 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title: 'Article 1 Himanshu',
+    heading: 'artile-one',
+    date: 'August 17 2017',
+    content: ` <p>
+                    This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.
+                    
+                </p>
+                <p>
+                    This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.
+                    
+                </p>
+                <p>
+                    This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.This is content for my first web app.. Hala Madrid . Real Madrid won the spanish super cup.
+                    </p> `
+    
+};
+
+function createTemplate (data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmlTemplate= `<html>
+        <head>
+            <title>
+                ${title} 
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+                </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
